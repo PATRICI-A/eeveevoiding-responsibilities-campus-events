fb0a40627d08d07300b3e01bfa5e3959e7bb0556
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-04-29 16:35:36 -0500

    confifuracion inicial

---
+++ src/main/java/edu/eci/patricia/DOWS_patricia/eeveevoiding_responsibilities_campus_events/EeveevoidingResponsibilitiesCampusEventsApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.patricia.DOWS_patricia.eeveevoiding_responsibilities_campus_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeveevoidingResponsibilitiesCampusEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeveevoidingResponsibilitiesCampusEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/patricia/DOWS_patricia/eeveevoiding_responsibilities_campus_events/EeveevoidingResponsibilitiesCampusEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.patricia.DOWS_patricia.eeveevoiding_responsibilities_campus_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeveevoidingResponsibilitiesCampusEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
