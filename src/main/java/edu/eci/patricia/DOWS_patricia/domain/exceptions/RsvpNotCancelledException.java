186a9144dd4ab8cc28343984aa93eda1f5e7174b
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-05-10 20:33:20 -0500

    exepciones

---
+++ src/main/java/edu/eci/patricia/DOWS_patricia/domain/exceptions/RsvpNotCancelledException.java
@@ -0,0 +1,7 @@
+package edu.eci.patricia.DOWS_patricia.domain.exceptions;
+
+public class RsvpNotCancelledException extends RuntimeException {
+    public RsvpNotCancelledException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/edu/eci/patricia/DOWS_patricia/domain/exceptions/RsvpNotConfirmedException.java
@@ -0,0 +1,7 @@
+package edu.eci.patricia.DOWS_patricia.domain.exceptions;
+
+public class RsvpNotConfirmedException extends RuntimeException {
+    public RsvpNotConfirmedException(String message) {
+        super(message);
+    }
+}
